3690ba6297ff7600e2791ee94764656e0213d5ff
wNv-paCe <pace.ap@fastmail.com>
2026-09-11 22:54:04 -0600

    fix: delete user issue

--- backend/tests/test_comments.py
+++ backend/tests/test_comments.py
@@ -46,7 +46,7 @@ async def test_create_comment_unauthorized(client: AsyncClient):
     )
 
     assert response.status_code == 401
-    assert response.json()["detail"] == "Invalid or expired token"
+    assert response.json()["detail"] == "Access token cookie missing"
 
 
 @pytest.mark.anyio
